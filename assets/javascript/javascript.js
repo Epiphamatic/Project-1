e9c4917d1db4fbf79da24ec2a53b19cd0e67689e
Milon <miloncl@gmail.com>
2018-10-04 21:10:46 -0700

    fucntioning drop down

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,38 @@
-var queryUrl = "https://api.nomics.com/v1/prices?key=2dc3cbe885c38fd8b528ac761e8ad9f1"
+var queryUrl = "https://api.nomics.com/v1/exchange-markets/prices?key=2dc3cbe885c38fd8b528ac761e8ad9f1&quote=USD"
 
-$.ajax({
-    url : queryUrl,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-});
+dropdown("binance");
+dropdown("bitfinex");
+dropdown("gateio");
+dropdown("hitbtc");
+dropdown("kraken");
+dropdown("poloniex");
+
+
+function dropdown (object)
+{
+    $.ajax({
+        url : queryUrl,
+        method: "GET"
+    }).then(function(response){
+    $("." + object ).on('click', function(){
+
+        $(".drop").remove();
+        for(i = 0; i < response.length; i++)
+        if (response[i].quote === 'USDT' || response[i].quote === 'USD'){
+    
+            console.log(response[i]);
+    
+            
+                if(response[i].exchange === object)
+                {
+    
+                    var a = $("<a class = 'dropdown-item drop' >");
+                    a.text(response[i].base);
+    
+                    $(".base").append(a);
+                }
+            
+            }
+        });
+    });
+}
