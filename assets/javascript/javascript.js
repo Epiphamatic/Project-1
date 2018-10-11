a40f91b7464762565014a328d75392f47b7d8681
Tri Pham <tjpham25@gmail.com>
2018-10-10 18:54:19 -0700

    Appending

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+// Firebase
+
+var config = {
+    apiKey: "AIzaSyC3qqQCLWKVU-kisz6cJuaR725aCNqQG5I",
+    authDomain: "crypton-d95b2.firebaseapp.com",
+    databaseURL: "https://crypton-d95b2.firebaseio.com",
+    projectId: "crypton-d95b2",
+    storageBucket: "crypton-d95b2.appspot.com",
+    messagingSenderId: "249923183261"
+  };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
 var queryUrl = "https://api.nomics.com/v1/exchange-markets/prices?key=2dc3cbe885c38fd8b528ac761e8ad9f1&quote=USD"
 
 dropdown("binance");
@@ -44,35 +59,90 @@ function dropdown (object)
 
 function coinClick() {
 
-    $(".drop").click(function() {
+    var coinName = $(this).attr('data-name');
 
-        var coinCard = $("<div class = 'card coinCard'>");
+    console.log(coinName);
 
-        var coinName = $(this).attr('data-name');
+    var coinValue = $(this).attr('data-val');
 
-        console.log(coinName);
+    console.log(coinValue);
 
-        var coinValue = $(this).attr('data-val');
+    var newCoin= {
 
-        console.log(coinValue);
-        
-        var coinNameText = $("<div class = 'coinName'>" + coinName + "</div>");
+        coinName: coinName,
 
-        var coinValueText = $("<div class = 'coinValue'> $ " + coinValue + "</div>");
+        coinValue: coinValue,
 
-        coinCard.append(coinNameText, coinValueText);
+    };
 
-        console.log(coinCard);
-
-        $("#coinRow").append(coinCard);
-
-        coinCard.clear();
-
-    });
+    var user = firebase.auth().currentUser;
+    console.log(user);
+    var ref = firebase.database().ref('users/' + user);
+    ref.set(newCoin);
 
 }
 
+// database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
+//     var coinCard = $("<div class = 'card coinCard'>");
+
+//     var coinNameText = $("<div class = 'coinName'>" + childSnapshot.val().coinName + "</div>");
+
+//     var coinValueText = $("<div class = 'coinValue'> $ " + childSnapshot.val().coinValue + "</div>");
+
+//     coinCard.append(coinNameText, coinValueText);
+
+//     $("#coinRow").append(coinCard);
+
+// });
+
+const txtEmail = $("#txtEmail");
+const txtPassword = $("#txtPassword");
+const btnLogin = $("#btnLogin");
+const btnSignUp = $("#btnSignUp");
+const btnLogout = $("#btnLogout");
+var userId = "";
+
+$(btnLogin).on("click", function() {
+
+    const email = txtEmail.val().trim();
+    const pass = txtPassword.val().trim();
+    const auth = firebase.auth();
+    userId = txtEmail.val().trim();
+
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+});
+
+$(btnSignUp).on("click", function() {
+
+    const email = txtEmail.val().trim();
+    const pass = txtPassword.val().trim();
+    const auth = firebase.auth();
+    userId = txtEmail.val().trim();
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+});
+
+$(btnLogout).on("click", function() {
+
+    firebase.auth().signOut();
+    userId = "";
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+
+    if (firebaseUser) {
+        console.log(firebaseUser);
+        btnLogout.removeClass("hide");
+    } else {
+        console.log("Not Logged In");
+        btnLogout.addClass("hide");
+    }
+});
 
 $(document).on("click", ".drop", coinClick);
 
